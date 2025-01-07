3952cb1c7e86307b23a2d60392679db8897f4671
benlaguroun <benlaguroun@gmail.com>
2025-01-07 17:23:11 +0100

    add pictures and mp3 fils

--- src/data/vocabulaireData.js
+++ src/data/vocabulaireData.js
@@ -1,20 +1,109 @@
 const vocabulaireData = [
+  // Words for A
   {
-    name: "Pomme",
-    image: "vocabulaireData/images/apple.jpg",
-    audio: "vocabulaireData/audio/apple.mp3",
+    name: "Abricot",
+    image: "vocabulaireData/images/abricot.jpg",
+    audio: "vocabulaireData/audio/abricot.mp3",
   },
   {
-    name: "chat",
-    image: "/images/cat.jpg",
-    audio: "/audio/cat.mp3",
+    name: "Abeille",
+    image: "vocabulaireData/images/abeille.jpg",
+    audio: "vocabulaireData/audio/abeille.mp3",
   },
   {
-    name: "Livre",
-    image: "/images/book.jpg",
-    audio: "/audio/book.mp3",
+    name: "Avion",
+    image: "vocabulaireData/images/Avion.jpg",
+    audio: "vocabulaireData/audio/avion.mp3",
   },
-  // Add more vocabulary items here
+  {
+    name: "Arbre",
+    image: "vocabulaireData/images/arbre.jpg",
+    audio: "vocabulaireData/audio/arbre.mp3",
+  },
+  {
+    name: "Ananas",
+    image: "vocabulaireData/images/ananas.jpg",
+    audio: "vocabulaireData/audio/ananas.mp3",
+  },
+  {
+    name: "Âne",
+    image: "vocabulaireData/images/ane.jpg",
+    audio: "vocabulaireData/audio/ane.mp3",
+  },
+  {
+    name: "Animal",
+    image: "vocabulaireData/images/animal.jpg",
+    audio: "vocabulaireData/audio/animal.mp3",
+  },
+  {
+    name: "Ami",
+    image: "vocabulaireData/images/ami.jpg",
+    audio: "vocabulaireData/audio/ami.mp3",
+  },
+  {
+    name: "Araignée",
+    image: "vocabulaireData/images/araignee.jpg",
+    audio: "vocabulaireData/audio/araignee.mp3",
+  },
+  {
+    name: "Arc-en-ciel",
+    image: "vocabulaireData/images/arc-en-ciel.jpg",
+    audio: "vocabulaireData/audio/arc-en-ciel.mp3",
+  },
+
+  // Words for B
+  {
+    name: "Banane",
+    image: "vocabulaireData/images/banane.jpg",
+    audio: "vocabulaireData/audio/banane.mp3",
+  },
+  {
+    name: "Bateau",
+    image: "vocabulaireData/images/bateau.jpg",
+    audio: "vocabulaireData/audio/bateau.mp3",
+  },
+  {
+    name: "Bébé",
+    image: "vocabulaireData/images/bebe.jpg",
+    audio: "vocabulaireData/audio/bebe.mp3",
+  },
+  {
+    name: "Bonbon",
+    image: "vocabulaireData/images/bonbon.jpg",
+    audio: "vocabulaireData/audio/bonbon.mp3",
+  },
+  {
+    name: "Ballon",
+    image: "vocabulaireData/images/ballon.jpg",
+    audio: "vocabulaireData/audio/ballon.mp3",
+  },
+  {
+    name: "Boîte",
+    image: "vocabulaireData/images/boite.jpg",
+    audio: "vocabulaireData/audio/boite.mp3",
+  },
+  {
+    name: "Brouette",
+    image: "vocabulaireData/images/brouette.jpg",
+    audio: "vocabulaireData/audio/brouette.mp3",
+  },
+  {
+    name: "Brosse",
+    image: "vocabulaireData/images/brosse.jpg",
+    audio: "vocabulaireData/audio/brosse.mp3",
+  },
+  {
+    name: "Bougie",
+    image: "vocabulaireData/images/bougie.jpg",
+    audio: "vocabulaireData/audio/bougie.mp3",
+  },
+  {
+    name: "Baguette",
+    image: "vocabulaireData/images/baguette.jpg",
+    audio: "vocabulaireData/audio/baguette.mp3",
+  },
+
+  // Add more letters similarly
 ];
 
 export default vocabulaireData;
