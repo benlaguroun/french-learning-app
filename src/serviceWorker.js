d6217f614e24174d84d87639d4d40c2197fa6e83
benlaguroun <benlaguroun@gmail.com>
2025-02-14 22:09:16 +0100

    fix register problem

---
+++ src/serviceWorker.js
@@ -0,0 +1,42 @@
+// serviceWorker.js
+const CACHE_NAME = "pwa-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/static/js/bundle.js",
+  "/images/present-tense-mindmap.jpg",
+  "/images/gendered-nouns-mindmap.jpg",
+];
+
+// Install Service Worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch Cached Files
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Update Cache
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 
-// Replace with your Supabase project credentials
-const SUPABASE_URL = "https://rhmjgzbjufondcyczgyq.supabase.co";
-const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJobWpnemJqdWZvbmRjeWN6Z3lxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg4NjgyODAsImV4cCI6MjA1NDQ0NDI4MH0.Sx1hiVq_ohHikSUvDFvpWJkbPszq_Q9K8nFZ-TVPkDY";
+// Load environment variables
+const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.REACT_APP_SUPABASE_ANON_KEY;
+
+// Ensure environment variables are set
+if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
+  console.error("Missing Supabase environment variables.");
+}
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
